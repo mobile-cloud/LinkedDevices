e4da71b333200ce6681c621bc139465772332daf
Joannazyx <joannazyx@gmail.com>
2014-08-19 20:59:10 +0800

    add general serial reader by alljoyn
    读取串口处发送的值

---
+++ AbstractDeviceByAllJoyn/src/org/onem2m/abstdev/ITemp.java
@@ -0,0 +1,12 @@
+package org.onem2m.abstdev;
+
+import org.alljoyn.bus.BusException;
+import org.alljoyn.bus.annotation.BusInterface;
+import org.alljoyn.bus.annotation.BusMethod;
+
+@BusInterface (name = "org.abstdev.businterface.temp")
+public interface ITemp {
+	@BusMethod
+	public int getTemp() throws BusException;
+	
+}

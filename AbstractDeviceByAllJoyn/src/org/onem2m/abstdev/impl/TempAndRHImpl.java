2f9521712a98bde7bd60dbcd8b03ea9c42d93a1d
Jue Huang <huangjuebest@163.com>
2014-09-04 22:52:00 +0800

    add buffer for TempAndRHImpl

--- AbstractDeviceByAllJoyn/src/org/onem2m/abstdev/impl/TempAndRHImpl.java
+++ AbstractDeviceByAllJoyn/src/org/onem2m/abstdev/impl/TempAndRHImpl.java
@@ -6,20 +6,33 @@ import org.onem2m.abstdev.ITempAndRH;
 import org.onem2m.abstdev.constant.NativeLib;
 
 class TempAndRHImpl implements ITempAndRH, BusObject{
-//	static{
-//		System.loadLibrary(NativeLib.TEMP_AND_RH);
-//	}
+	static{
+		System.loadLibrary(NativeLib.TEMP_AND_RH);
+	}
+	
+	private int buffTemp;
+	private int buffRH;
+	
+	public TempAndRHImpl(){
+		buffTemp=-99;
+		buffRH=-99;
+	}
+	
 	@Override
 	public int getTemp() throws BusException {
-		return getTempNative();
+		return buffTemp;
 	}
 
 	@Override
 	public int getRH() throws BusException {
-		return getRHNative();
+		return buffRH;
+	}
+	
+	public void bufferTempAndRH(){
+		buffTemp=getTempNative();
+		buffRH=getRHNative();
 	}
 	
 	private native int getTempNative();
 	private native int getRHNative();
-	
 }

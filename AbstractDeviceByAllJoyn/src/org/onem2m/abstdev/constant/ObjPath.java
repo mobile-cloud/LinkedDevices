ca126703aa2822814e81c4ef52b03cbba4523d6e
Jue Huang <huangjuebest@163.com>
2015-02-10 16:30:11 +0800

    adjust code format

--- AbstractDeviceByAllJoyn/src/org/onem2m/abstdev/constant/ObjPath.java
+++ AbstractDeviceByAllJoyn/src/org/onem2m/abstdev/constant/ObjPath.java
@@ -2,15 +2,8 @@ package org.onem2m.abstdev.constant;
 
 public class ObjPath {
 	public static final String TEMP_AND_RH="/myTempAndRH";
-
 	public static final String TEMP="/myTemp";
-
 	public static final String AIR_CONDITIONER="/myAirConditioner";
-
-	
 	public static final String MULTISENSOR="/myMultiSensor";
-
-
 	public static final String HUMIDIFIER="/myHumidifier";
-
 }
